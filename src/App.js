ec44c59ba1447e8bab02ce8f1a29932f1a25e831
mefegiorgi <33478970+mefegiorgi@users.noreply.github.com>
2020-02-26 18:45:22 +0400

    დავამატე ლერი

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. lalalalalala  ყვერებიანი ძროხა !! yveri
+          ლერი!!!!!!!!!!!!
         </p>
         <a
           className="App-link"
